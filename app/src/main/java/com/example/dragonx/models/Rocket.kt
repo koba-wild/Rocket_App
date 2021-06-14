bec4e1fc1c1a133c9d841c9d1be884891acad309
Эвгеній  Кобахідзе <seraphjackson@gmail.com>
2021-06-14 13:25:30 +0300

    My DragonX

---
+++ app/src/main/java/com/example/dragonx/models/Rocket.kt
@@ -0,0 +1,20 @@
+package com.example.dragonx.models
+
+import java.net.URL
+
+class Rocket {
+    @JvmField
+    var name: String? = null
+    @JvmField
+    var description: String? = null
+    @JvmField
+    var link: String? = null
+    @JvmField
+    var height: String? = null
+    @JvmField
+    var mass: String? = null
+    @JvmField
+    var year: String? = null
+    @JvmField
+    var image: URL? = null
+}
